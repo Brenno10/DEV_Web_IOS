28f58d94dfbeeb29659f51f1cea919162aaefdab
Brenno10 <brenno10000@outlook.com>
2022-04-27 20:58:26 -0300

    Exercício 02 JS linha 5 modificada

--- Java Script/Aula 02 - Console, variáveis e operadores/Exercício 02/main.js
+++ Java Script/Aula 02 - Console, variáveis e operadores/Exercício 02/main.js
@@ -2,6 +2,6 @@ var nome = 'Brenno';
 var altura = 1.7;
 var peso = 80;
 var imc = peso / (altura ^ 2);
-const templateString = `${nome} possui altura ${altura} e peso ${peso}, seu IMC é ${imc}`;
+const templateString = `${nome} possui altura ${altura}m e peso ${peso}kg, seu IMC é ${imc}`;
 
 console.log(templateString);
