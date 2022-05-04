6f4768bdb69c142df6660d4dd57a4476f923cf34
Brenno10 <brenno10000@outlook.com>
2022-05-03 21:38:59 -0300

    Exercício JS 04 (v2) modificado

--- Java Script/Aula 04 - Arrays/Exercício 04 (v2)/main.js
+++ Java Script/Aula 04 - Arrays/Exercício 04 (v2)/main.js
@@ -5,9 +5,11 @@ const alunos = [
     'Coen Strong',
     'Habib Webber',
 ];
+console.log(alunos);
 
 const info = [
     (Score = ['8', '6', '9', '7', '8']),
     (Birth = ['2006', '2005', '2007', '2006', '2005']),
     (Class = ['2', '3', '1', '2', '3']),
 ];
+console.log(info);
