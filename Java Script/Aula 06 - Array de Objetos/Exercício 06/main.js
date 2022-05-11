fb431079ad07a334527691c8b8f96faee3220a9c
Brenno10 <brenno10000@outlook.com>
2022-05-10 21:36:52 -0300

    Exercício 06 modificado

--- Java Script/Aula 06 - Array de Objetos/Exercício 06/main.js
+++ Java Script/Aula 06 - Array de Objetos/Exercício 06/main.js
@@ -120,17 +120,17 @@ alunos[4].media = media;
 
 // console.log()
 console.log(
-    `Aluna ${alunos[0].firstName} ${alunos[0].lastName}, ${alunos[0].age}, da turma ${alunos[0].turma} com as notas ${alunos[0].notas}, mora em ${alunos[0].endereco.rua} N°${alunos[0].endereco.numero} e teve a média de ${alunos[0].media}/7, portanto foi ${alunos[0].pass}`
+    `Aluna ${alunos[0].firstName} ${alunos[0].lastName}, ${alunos[0].age} anos, da turma ${alunos[0].turma} com as notas ${alunos[0].notas}, mora em ${alunos[0].endereco.rua} N°${alunos[0].endereco.numero} e teve a média de ${alunos[0].media}/7, portanto foi ${alunos[0].pass}`
 );
 console.log(
-    `Aluna ${alunos[1].firstName} ${alunos[1].lastName}, ${alunos[1].age}, da turma ${alunos[1].turma} com as notas ${alunos[1].notas}, mora em ${alunos[1].endereco.rua} N°${alunos[1].endereco.numero} e teve a média de ${alunos[1].media}/7, portanto foi ${alunos[1].pass}`
+    `Aluna ${alunos[1].firstName} ${alunos[1].lastName}, ${alunos[1].age} anos, da turma ${alunos[1].turma} com as notas ${alunos[1].notas}, mora em ${alunos[1].endereco.rua} N°${alunos[1].endereco.numero} e teve a média de ${alunos[1].media}/7, portanto foi ${alunos[1].pass}`
 );
 console.log(
-    `Aluno ${alunos[2].firstName} ${alunos[2].lastName}, ${alunos[2].age}, da turma ${alunos[2].turma} com as notas ${alunos[2].notas}, mora em ${alunos[2].endereco.rua} N°${alunos[2].endereco.numero} e teve a média de ${alunos[2].media}/7, portanto foi ${alunos[2].pass}`
+    `Aluno ${alunos[2].firstName} ${alunos[2].lastName}, ${alunos[2].age} anos, da turma ${alunos[2].turma} com as notas ${alunos[2].notas}, mora em ${alunos[2].endereco.rua} N°${alunos[2].endereco.numero} e teve a média de ${alunos[2].media}/7, portanto foi ${alunos[2].pass}`
 );
 console.log(
-    `Aluna ${alunos[3].lastName} ${alunos[3].firstName}, ${alunos[3].age}, da turma ${alunos[3].turma} com as notas ${alunos[3].notas}, mora em ${alunos[3].endereco.rua} N°${alunos[3].endereco.numero} e teve a média de ${alunos[3].media}/7, portanto foi ${alunos[3].pass}`
+    `Aluna ${alunos[3].lastName} ${alunos[3].firstName}, ${alunos[3].age} anos, da turma ${alunos[3].turma} com as notas ${alunos[3].notas}, mora em ${alunos[3].endereco.rua} N°${alunos[3].endereco.numero} e teve a média de ${alunos[3].media}/7, portanto foi ${alunos[3].pass}`
 );
 console.log(
-    `Aluno ${alunos[4].firstName} ${alunos[4].lastName}, ${alunos[4].age}, da turma ${alunos[4].turma} com as notas ${alunos[4].notas}, mora em ${alunos[4].endereco.rua} N°${alunos[4].endereco.numero} e teve a média de ${alunos[4].media}/7, portanto foi ${alunos[4].pass}`
+    `Aluno ${alunos[4].firstName} ${alunos[4].lastName}, ${alunos[4].age} anos, da turma ${alunos[4].turma} com as notas ${alunos[4].notas}, mora em ${alunos[4].endereco.rua} N°${alunos[4].endereco.numero} e teve a média de ${alunos[4].media}/7, portanto foi ${alunos[4].pass}`
 );
