21a0defb84ef163b72eb9fc8bcf35e049ce20145
Brenno10 <brenno10000@outlook.com>
2022-05-12 21:52:50 -0300

    modificações na aula JS 08

--- Java Script/Aula 08 - Laços de repetição/Exercício 08/main.js
+++ Java Script/Aula 08 - Laços de repetição/Exercício 08/main.js
@@ -1,5 +1,6 @@
 const base = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+// Verifica qual multiplo esta sendo usado
 for (let i = 0; i <= base.length; i++) {
     if (i == 0) {
         console.log(`Todo Multiplo de ${i} é igual a 0`);
@@ -9,6 +10,7 @@ for (let i = 0; i <= base.length; i++) {
     }
 }
 
+// Multiplicador
 function contador(base) {
     let i = 0;
     while (i <= 10) {
--- Java Script/Aula 08 - Laços de repetição/main.js
+++ Java Script/Aula 08 - Laços de repetição/main.js
@@ -53,3 +53,28 @@ const frutas = ['maca', 'laranja', 'pera', 10];
 for (let j = 0; j < frutas.length; j++) {
     console.log(frutas[j]);
 }
+
+// Array de objetos e laços
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        isCompleted: true,
+    },
+    {
+        id: 3,
+        text: 'Dentist appt',
+        isCompleted: false,
+    },
+];
+// For overloop
+for (let t of todos) {
+    console.log(t);
+    console.log(t.text);
+    console.log(t.id);
+}
