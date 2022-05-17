395108952c983327aaf53fea43b1c7e075410ed6
Brenno10 <brenno10000@outlook.com>
2022-05-17 04:29:37 -0300

    Exercício JS em sala finalizado

--- Java Script/Aula 09 - Métodos de alto nível para manipular arrays/Exercício em sala/main.js
+++ Java Script/Aula 09 - Métodos de alto nível para manipular arrays/Exercício em sala/main.js
@@ -2,38 +2,61 @@ const alunos = [
     {
         nome: 'Nora',
         idade: 16,
-        turma: 2,
+        gender: 'female',
         nota: 5,
     },
     {
         nome: 'Sellene',
         idade: 17,
-        turma: 3,
+        gender: 'female',
         nota: 9,
     },
     {
         nome: 'Mikael',
         idade: 16,
-        turma: 2,
+        gender: 'male',
         nota: 4,
     },
     {
         nome: 'Lutz',
         idade: 15,
-        turma: 1,
+        gender: 'male',
         nota: 7,
     },
 ];
 
-alunos.forEach((name) => console.log(name.nome));
+// Verifica o nome de cada aluno e aluna por gênero e suas notas
+alunos.forEach((name) => {
+    if (name.gender == 'female') {
+        console.log(`Aluna: ${name.nome}, Nota: ${name.nota}`);
+    }
+    else {
+        console.log(`Aluno: ${name.nome}, Nota: ${name.nota}`);
+    }
+});
 
+// Filtra todos os alunos com nota superior a 6
 let highScore = alunos.filter((highScore) => {
     return highScore.nota > 6;
 });
-console.log(highScore);
 
-console.log(
-    alunos.find((lowScore) => {
-        return lowScore.nota < 5;
-    })
-);
+highScore.forEach((Name) => {
+    if (Name.gender == 'female') {
+        console.log(`A aluna ${Name.nome} obteu nota maior que 6`);
+    }
+    else {
+        console.log(`O aluno ${Name.nome} obteu nota maior que 6`);
+    }
+});
+
+// Acha o aluno com menor nota
+let lowScore = alunos.find((lowScore) => {
+    return lowScore.nota < 5;
+});
+
+if (lowScore.gender == 'female') {
+    console.log(`A aluna ${lowScore.nome} obteve nota menor que 5`);
+}
+else {
+    console.log(`O aluno ${lowScore.nome} obteve nota menor que 5`);
+}
