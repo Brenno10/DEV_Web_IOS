c637697b0a82e1f9463b0797009fc55b718ed2cd
Brenno10 <brenno10000@outlook.com>
2022-05-25 21:26:28 -0300

    Exercício JS 14 finalizado

--- Java Script/Aula 14 - JavaScript HTML DOM - Parte 04/Exercício 14/assets/scripts/main.js
+++ Java Script/Aula 14 - JavaScript HTML DOM - Parte 04/Exercício 14/assets/scripts/main.js
@@ -23,6 +23,9 @@ let noEmail = document.getElementById('noEmail');
 let noBirthday = document.getElementById('noBirthday');
 let noId = document.getElementById('noId');
 
+// Variaveis de funções
+let idNum = 1;
+
 // Apaga apenas 1 input do formulário
 function formClear(i) {
     let input = document.getElementsByTagName('input');
@@ -49,35 +52,50 @@ function createNewUser() {
     let validEmailTest = new RegExp(/.*@.*\..*/i);
 
     if (fixedUserName.trim() === '') {
-        noName.classList.add('error');
-        noName.innerText = 'Por favor, digite seu nome';
-        setTimeout(() => noName.remove(), 3000);
+        let noNameHere = document.createElement('p');
+        noName.appendChild(noNameHere);
+
+        noNameHere.classList.add('error');
+        noNameHere.innerText = 'Por favor, digite seu nome';
+        setTimeout(() => noNameHere.remove(), 3000);
         formClear(0);
     } else if (fixedUserBirthday.trim() === '') {
-        noBirthday.classList.add('error');
-        noBirthday.innerText = 'Por favor, digite sua data de nascimento';
-        setTimeout(() => noBirthday.remove(), 3000);
+        let noBirthdayHere = document.createElement('p');
+        noBirthday.appendChild(noBirthdayHere);
+
+        noBirthdayHere.classList.add('error');
+        noBirthdayHere.innerText = 'Por favor, digite sua data de nascimento';
+        setTimeout(() => noBirthdayHere.remove(), 3000);
         formClear(1);
     } else if (year - fixedUserAge.getFullYear() < 18) {
-        noBirthday.classList.add('error');
-        noBirthday.innerText = 'Menores de 18 anos não podem ser adicionados';
-        setTimeout(() => noBirthday.remove(), 3000);
+        let notEnoughAge = document.createElement('p');
+        noBirthday.appendChild(notEnoughAge);
+
+        notEnoughAge.classList.add('error');
+        notEnoughAge.innerText = 'Menores de 18 anos não podem ser adicionados';
+        setTimeout(() => notEnoughAge.remove(), 3000);
         formClear(1);
     } else if (fixedUserEmail.trim() === '') {
-        noEmail.classList.add('error');
-        noEmail.innerText = 'Por favor, digite seu e-mail';
-        setTimeout(() => noEmail.remove(), 3000);
+        let noEmailHere = document.createElement('p');
+        noEmail.appendChild(noEmailHere);
+
+        noEmailHere.classList.add('error');
+        noEmailHere.innerText = 'Por favor, digite seu e-mail';
+        setTimeout(() => noEmailHere.remove(), 3000);
         formClear(2);
     } else if (!validEmailTest.test(userEmail.value)) {
-        noEmail.classList.add('error');
-        noEmail.innerText = 'Email invalido';
-        setTimeout(() => noEmail.remove(), 3000);
+        let noValidEmail = document.createElement('p');
+        noEmail.appendChild(noValidEmail);
+
+        noValidEmail.classList.add('error');
+        noValidEmail.innerText = 'Email invalido';
+        setTimeout(() => noValidEmail.remove(), 3000);
         formClear(2);
     } else {
         let newUser = document.createElement('li');
         newUser.appendChild(
             document.createTextNode(
-                `Nome ${fixedUserName} : data de nascimento ${userBirthday.value} : e-mail ${userEmail.value} : id `
+                `Nome ${fixedUserName} : data de nascimento ${fixedUserBirthday} : e-mail ${fixedUserEmail}}`
             )
         );
         users.appendChild(newUser);
@@ -86,4 +104,29 @@ function createNewUser() {
 }
 
 // Exclui um usuario existente pelo id
-function deleteExistingUser() {}
+function deleteExistingUser() {
+    let getExistingUser = users.getElementsByTagName('li');
+    let allExistingUsers = Array.from(getExistingUser);
+    let getUserID = userId.value;
+
+    if (isNaN(getUserID)) {
+        let idNaN = document.createElement('p');
+        noId.appendChild(idNaN);
+
+        idNaN.classList.add('error');
+        idNaN.innerText = 'Por favor, digite um número';
+        setTimeout(() => idNaN.remove(), 3000);
+        formClear(3);
+    } else if (allExistingUsers[getUserID - 1] === undefined) {
+        let dontExist = document.createElement('p');
+        noId.appendChild(dontExist);
+
+        dontExist.classList.add('error');
+        dontExist.innerText = `${getUserID} não existe`;
+        setTimeout(() => dontExist.remove(), 3000);
+        formClear(3);
+    } else {
+        allExistingUsers[getUserID - 1].remove();
+        formClear(3);
+    }
+}
