0c77d8f8591fc174d321ceac59f5d3b975967c80
Brenno10 <brenno10000@outlook.com>
2022-05-26 21:41:59 -0300

    Aula react 01 e Exercício 15

--- Java Script/Aula 15 - JavaScript HTML DOM - Parte 04/Exercício 15/scripts/main.js
+++ Java Script/Aula 15 - JavaScript HTML DOM - Parte 04/Exercício 15/scripts/main.js
@@ -1,13 +1,100 @@
-let Arroz = 18.0;
+let itemName = document.getElementById('itemName');
+let itemPrice = document.getElementById('itemPrice');
+let newItemBtnList = document.getElementById('items');
+let addNewItem = document.getElementById('addNewItem');
 
-let addArroz = document.getElementById('addArroz');
-let items = document.getElementById('items');
-let total = document.getElementById('totalPrice');
+class PriceTable {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+        this.createNewItem();
+    }
 
-addArroz.addEventListener('click', addNewItem);
+    createNewItem() {
+        let newItem = document.createElement('li');
+        let btnDiv = document.createElement('div');
 
-function addNewItem() {
-    let newItem = document.createElement('li');
-    items.appendChild(newItem);
-    newItem.innerText = 'teste';
+        newItem.innerText = `${this.name} ${this.price}`;
+        newItem.classList = 'd-block p-2 list-group-item';
+        btnDiv.classList = '';
+
+        this.addAllButtons(btnDiv);
+
+        newItem.appendChild(btnDiv);
+        newItemBtnList.appendChild(newItem);
+    }
+
+    addAllButtons(whereToPlace) {
+        // Quantidade atual de itens
+        let moreItemValue = document.createElement('p');
+        let currentValue = 1;
+
+        moreItemValue.innerText = `${currentValue}`;
+        moreItemValue.classList = 'list-group-item float-end';
+
+        // Botão que aumenta a quantidade de itens
+        let moreItemBtn = document.createElement('button');
+
+        moreItemBtn.innerText = '+';
+        moreItemBtn.classList =
+            'btn btn-success btn-sm list-group-item float-end';
+
+        // Botão que diminui a quantidade de itens
+        let lessItemBtn = document.createElement('button');
+
+        lessItemBtn.innerText = '-';
+        lessItemBtn.classList =
+            'btn btn-warning btn-sm list-group-item float-end';
+
+        // Botão que remove o item
+        let removeItemBtn = document.createElement('button');
+
+        removeItemBtn.innerText = 'X';
+        removeItemBtn.classList =
+            'btn btn-sm btn-danger list-group-item float-end';
+
+        whereToPlace.appendChild(moreItemBtn);
+        whereToPlace.appendChild(lessItemBtn);
+        whereToPlace.appendChild(removeItemBtn);
+        whereToPlace.appendChild(moreItemValue);
+
+        // Eventos
+        moreItemBtn.addEventListener('click', moreItems);
+        lessItemBtn.addEventListener('click', lessItems);
+        removeItemBtn.addEventListener('click', removeItem);
+
+        function moreItems(e) {
+            e.preventDefault();
+
+            currentValue++;
+            if (currentValue === 0) {
+            }
+            moreItemValue.innerText = `${currentValue}`;
+        }
+
+        function lessItems(e) {
+            e.preventDefault();
+
+            currentValue--;
+            if (currentValue === 0) {
+            }
+            moreItemValue.innerText = `${currentValue}`;
+        }
+
+        function removeItem(e) {
+            e.preventDefault();
+
+            currentValue = 0;
+            if (currentValue === 0) {
+            }
+            moreItemValue.innerText = `${currentValue}`;
+        }
+    }
+}
+
+addNewItem.addEventListener('click', prevent);
+
+function prevent(e) {
+    e.preventDefault();
+    new PriceTable(itemName.value, itemPrice.value);
 }
