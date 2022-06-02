827cb2c25cdeb8f60d2449dfc859119dfe1cb3b8
Brenno10 <brenno10000@outlook.com>
2022-06-01 21:48:26 -0300

    Aula React 02 nome da pasta modificada e Aula React 03 adicionada

---
+++ React/Aula 03 - Componentes Funcionais/03_ComponentesFuncionais/tema_03_conta_cliques/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Exemplo from './components/Exemplo';
+import Header from './components/Header';
+
+function App() {
+    return (
+        <div className="App">
+            <Header nome="Brenno" />
+            <Exemplo />
+        </div>
+    );
+}
+
+export default App;
---
+++ React/Aula 03 - Componentes Funcionais/03_ComponentesFuncionais/tema_03_conta_cliques/src/components/Exemplo.jsx
@@ -0,0 +1,16 @@
+// componentes devem ter letra maiuscula
+import { useState } from 'react';
+
+const Exemplo = () => {
+    const [contador, setContador] = useState(0);
+    return (
+        <div>
+            <p>Você clicou {contador} vezes</p>
+            <button onClick={() => setContador(contador + 1)}>
+                Clique aqui
+            </button>
+        </div>
+    );
+};
+
+export default Exemplo;
