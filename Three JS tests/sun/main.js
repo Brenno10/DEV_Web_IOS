e9e2980bc284feb67bc205c84ad913616f26364f
Brenno10 <brenno10000@outlook.com>
2022-07-02 04:02:24 -0300

    escala modificada, anéis de saturno adicionados

--- Three JS tests/sun/main.js
+++ Three JS tests/sun/main.js
@@ -18,7 +18,7 @@ const canvas = document.getElementsByTagName('canvas')[0];
 const fov = 60;
 const aspect = window.innerWidth / window.innerHeight;
 const near = 0.01;
-const far = 1000000;
+const far = 10000000;
 
 //
 // inicializações
@@ -32,19 +32,19 @@ renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 const renderScene = new RenderPass(scene, camera);
 const bloomPass = new UnrealBloomPass(
     new THREE.Vector2(window.innerWidth, window.innerHeight),
-    1.5,
-    0.4,
-    0.85
+    2,
+    0,
+    0
 );
 
 // iniciando effect composer
 const bloomComposer = new EffectComposer(renderer);
 
 // iniciando luz ambiente
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.015);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.115);
 
 // iniciando ponto de luz
-const sunLight = new THREE.PointLight(0xffffff, 1.5);
+const sunLight = new THREE.PointLight(0xffffff, 1);
 
 // iniciando orbit controls
 const controls = new OrbitControls(camera, document.querySelector('canvas'));
@@ -59,11 +59,6 @@ renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
 renderer.setClearColor(0x000000, 0.0);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-// configurações do bloom pass
-bloomPass.threshold = 0;
-bloomPass.strength = 2;
-bloomPass.radius = 0;
 
 // configurações do effect composer
 bloomComposer.setSize(window.innerWidth, window.innerHeight);
@@ -80,21 +75,19 @@ controls.dampingFactor = 0.05;
 
 // sol
 const sun = new THREE.Mesh(
-    new THREE.IcosahedronGeometry(139.27, 50, 50),
+    new THREE.IcosahedronGeometry(69.55, 50),
     new THREE.MeshBasicMaterial({
-        map: new THREE.TextureLoader().load('./assets/textures/2k/2k_sun.jpg'),
+        map: new THREE.TextureLoader().load('./assets/textures/2k_sun.jpg'),
+        depthWrite: true,
+        transparent: false,
     })
 );
 
 // mercurio
 const mercury = new THREE.Mesh(
-    new THREE.SphereGeometry(0.48794, 50, 50),
+    new THREE.SphereGeometry(0.24394, 50, 50),
     new THREE.MeshPhongMaterial({
-        roughness: 1,
-        metalness: 0,
-        map: new THREE.TextureLoader().load(
-            './assets/textures/2k/2k_mercury.jpg'
-        ),
+        map: new THREE.TextureLoader().load('./assets/textures/2k_mercury.jpg'),
     })
 );
 mercury.receiveShadow = true;
@@ -102,12 +95,10 @@ mercury.castShadow = true;
 
 // vênus
 const venus = new THREE.Mesh(
-    new THREE.SphereGeometry(1.2104, 50, 50),
+    new THREE.SphereGeometry(0.60518, 50, 50),
     new THREE.MeshPhongMaterial({
-        roughness: 1,
-        metalness: 0,
         map: new THREE.TextureLoader().load(
-            './assets/textures/2k/2k_venus_surface.jpg'
+            './assets/textures/2k_venus_surface.jpg'
         ),
     })
 );
@@ -116,10 +107,8 @@ venus.castShadow = true;
 
 // terra
 const earth = new THREE.Mesh(
-    new THREE.SphereGeometry(1.2742, 50, 50),
+    new THREE.SphereGeometry(0.63781, 50, 50),
     new THREE.MeshPhongMaterial({
-        roughness: 1,
-        metalness: 0,
         map: new THREE.TextureLoader().load('./assets/textures/earthmap1.jpg'),
     })
 );
@@ -127,10 +116,8 @@ earth.receiveShadow = true;
 earth.castShadow = true;
 // nuvens da terra
 const clouds = new THREE.Mesh(
-    new THREE.SphereGeometry(1.2842, 50, 50),
+    new THREE.SphereGeometry(0.63881, 50, 50),
     new THREE.MeshPhongMaterial({
-        roughness: 1,
-        metalness: 0,
         map: new THREE.TextureLoader().load('./assets/textures/earthCloud.jpg'),
         transparent: true,
     })
@@ -138,11 +125,9 @@ const clouds = new THREE.Mesh(
 
 // lua
 const moon = new THREE.Mesh(
-    new THREE.SphereGeometry(0.34748, 50, 50),
+    new THREE.SphereGeometry(0.17381, 50, 50),
     new THREE.MeshPhongMaterial({
-        roughness: 5,
-        metalness: 0,
-        map: new THREE.TextureLoader().load('./assets/textures/2k/2k_moon.jpg'),
+        map: new THREE.TextureLoader().load('./assets/textures/2k_moon.jpg'),
     })
 );
 moon.receiveShadow = true;
@@ -150,11 +135,9 @@ moon.castShadow = true;
 
 // marte
 const mars = new THREE.Mesh(
-    new THREE.SphereGeometry(0.6779, 50, 50),
+    new THREE.SphereGeometry(0.33962, 50, 50),
     new THREE.MeshPhongMaterial({
-        roughness: 1,
-        metalness: 0,
-        map: new THREE.TextureLoader().load('./assets/textures/2k/2k_mars.jpg'),
+        map: new THREE.TextureLoader().load('./assets/textures/2k_mars.jpg'),
     })
 );
 mars.receiveShadow = true;
@@ -162,13 +145,9 @@ mars.castShadow = true;
 
 // jupiter
 const jupiter = new THREE.Mesh(
-    new THREE.SphereGeometry(13.982, 50, 50),
+    new THREE.SphereGeometry(7.1492, 50, 50),
     new THREE.MeshPhongMaterial({
-        roughness: 1,
-        metalness: 0,
-        map: new THREE.TextureLoader().load(
-            './assets/textures/2k/2k_jupiter.jpg'
-        ),
+        map: new THREE.TextureLoader().load('./assets/textures/2k_jupiter.jpg'),
     })
 );
 jupiter.receiveShadow = true;
@@ -176,40 +155,43 @@ jupiter.castShadow = true;
 
 // saturno
 const saturn = new THREE.Mesh(
-    new THREE.SphereGeometry(11.646, 50, 50),
+    new THREE.SphereGeometry(6.0268, 50, 50),
     new THREE.MeshPhongMaterial({
-        roughness: 1,
-        metalness: 0,
-        map: new THREE.TextureLoader().load(
-            './assets/textures/2k/2k_saturn.jpg'
-        ),
+        map: new THREE.TextureLoader().load('./assets/textures/2k_saturn.jpg'),
     })
 );
 saturn.receiveShadow = true;
 saturn.castShadow = true;
-// anéis
-const saturnRings = new THREE.Mesh(
-    new THREE.RingBufferGeometry(11, 50, 50),
-    new THREE.MeshBasicMaterial({
-        map: new THREE.TextureLoader().load(
-            './assets/textures/2k/2k_saturn_ring_alpha.png'
-        ),
-        transparent: true,
+// anéis de saturno
+const saturnRings = createSaturnRings();
+function createSaturnRings() {
+    const texture = new THREE.TextureLoader().load(
+        './assets/textures/2k_saturn_ring_alpha.png'
+    );
+    const material = new THREE.MeshPhongMaterial({
+        map: texture,
         side: THREE.DoubleSide,
-    })
-);
-saturnRings.receiveShadow = true;
-saturnRings.castShadow = true;
+        color: 0xffffff,
+        transparent: true,
+    });
+    const geometry = new THREE.RingBufferGeometry(6.6958, 14.022, 64);
+    var pos = geometry.attributes.position;
+    var v3 = new THREE.Vector3();
+    for (let i = 0; i < pos.count; i++) {
+        v3.fromBufferAttribute(pos, i);
+        geometry.attributes.uv.setXY(i, v3.length() < 14 ? 0 : 1, 1);
+    }
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    return mesh;
+}
 
 // urano
 const uranus = new THREE.Mesh(
-    new THREE.SphereGeometry(5.0724, 50, 50),
+    new THREE.SphereGeometry(2.5559, 50, 50),
     new THREE.MeshPhongMaterial({
-        roughness: 1,
-        metalness: 0,
-        map: new THREE.TextureLoader().load(
-            './assets/textures/2k/2k_uranus.jpg'
-        ),
+        map: new THREE.TextureLoader().load('./assets/textures/2k_uranus.jpg'),
     })
 );
 uranus.receiveShadow = true;
@@ -217,13 +199,9 @@ uranus.castShadow = true;
 
 // netuno
 const neptune = new THREE.Mesh(
-    new THREE.SphereGeometry(4.9244, 50, 50),
+    new THREE.SphereGeometry(2.4764, 50, 50),
     new THREE.MeshPhongMaterial({
-        roughness: 1,
-        metalness: 0,
-        map: new THREE.TextureLoader().load(
-            './assets/textures/2k/2k_neptune.jpg'
-        ),
+        map: new THREE.TextureLoader().load('./assets/textures/2k_neptune.jpg'),
     })
 );
 neptune.receiveShadow = true;
@@ -231,7 +209,7 @@ neptune.castShadow = true;
 
 // plano de fundo
 const stars = new THREE.Mesh(
-    new THREE.SphereGeometry(900000, 64, 64),
+    new THREE.SphereGeometry(9000000, 64, 64),
     new THREE.MeshBasicMaterial({
         map: new THREE.TextureLoader().load('./assets/textures/galaxy.jpg'),
         side: BackSide,
@@ -254,31 +232,47 @@ function redimensionar() {
 //
 // grupos
 const sunGroup = new THREE.Group();
-sunGroup.add(sun);
-sunGroup.add(sunLight);
+sunGroup.add(sun, sunLight);
 
 const earthGroup = new THREE.Group();
-earthGroup.add(earth);
-earthGroup.add(clouds);
+earthGroup.add(earth, clouds);
 
 const saturnGroup = new THREE.Group();
-saturnGroup.add(saturn);
-saturnGroup.add(saturnRings);
+saturnGroup.add(saturn, saturnRings);
 
 //
 // posições iniciais
 camera.position.set(0, 0, 400); // câmera
 stars.position.set(0, 0, 0); // plano de fundo
 sunGroup.position.set(0, 0, 0); // sol
+
 mercury.position.set(170, 0, 0);
+mercury.rotation.z = 0.01 * (Math.PI / 180);
+
 venus.position.set(180, 0, 0);
+venus.rotation.z = 177.36 * (Math.PI / 180);
+
 earthGroup.position.set(190, 0, 0);
+earth.rotation.z = 23 * (Math.PI / 180);
+
 moon.position.set(190, 3, 0);
+moon.rotation.z = 5.145 * (Math.PI / 180);
+
 mars.position.set(200, 0, 0);
+mars.rotation.z = 25 * (Math.PI / 180);
+
 jupiter.position.set(230, 0, 0);
+jupiter.rotation.z = 1.305 * (Math.PI / 180);
+
 saturnGroup.position.set(265, 0, 0);
+saturnGroup.rotation.z = 26.73 * (Math.PI / 180);
+saturnRings.rotation.x = 90 * (Math.PI / 180);
+
 uranus.position.set(290, 0, 0);
+uranus.rotation.z = 97.77 * (Math.PI / 180);
+
 neptune.position.set(310, 0, 0);
+neptune.rotation.z = 1.767975 * (Math.PI / 180);
 
 //
 // layers
@@ -300,19 +294,21 @@ neptune.layers.set(0);
 
 //
 // adicionando na cena
-scene.add(camera);
-scene.add(ambientLight);
-scene.add(stars);
-scene.add(sunGroup);
-scene.add(mercury);
-scene.add(venus);
-scene.add(earthGroup);
-scene.add(moon);
-scene.add(mars);
-scene.add(jupiter);
-scene.add(saturnGroup);
-scene.add(uranus);
-scene.add(neptune);
+scene.add(
+    camera,
+    ambientLight,
+    stars,
+    sunGroup,
+    mercury,
+    venus,
+    earthGroup,
+    moon,
+    mars,
+    jupiter,
+    saturnGroup,
+    uranus,
+    neptune
+);
 
 //
 // animações
@@ -320,9 +316,11 @@ function animation() {
     requestAnimationFrame(animation); // função responsavel pela animação
 
     controls.update();
+
     camera.layers.set(1);
     bloomComposer.render();
     renderer.clearDepth();
+
     camera.layers.set(0);
     renderer.render(scene, camera);
 }
